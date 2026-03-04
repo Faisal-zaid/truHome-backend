55857f70f8fa294f723abc48b331b28f2c607db6
Faisal Zaid <zaidalideveloper@gmail.com>
2026-03-04 16:21:58 +0300

    made models

---
+++ models.py
@@ -0,0 +1,51 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db=SQLAlchemy()
+
+#ADMIN
+class Admin(db.Model):
+    __tablename__="admins"
+
+    id=db.column(db.Integer, primary_key=True)
+    userName=db.column(db.Text, unique=True, nullable =False)
+    password=db.column(db.Text, unique=True, nullable=False)
+
+#products
+class Pajamas(db.model):
+    __tablename__="pajamas"   
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text, nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    image = db.Column(db.String(500), nullable=False)
+
+
+class Nightdress(db.Model):
+    __tablename__ = "nightdress"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text, nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    image = db.Column(db.String(500), nullable=False)
+
+
+class Rompers(db.Model):
+    __tablename__ = "rompers"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text, nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    image = db.Column(db.String(500), nullable=False)
+
+
+class Bathrobes(db.Model):
+    __tablename__ = "bathrobes"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text, nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    image = db.Column(db.String(500), nullable=False) 
