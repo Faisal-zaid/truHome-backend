46b28b9976c915990ba2db3fa98cb267cb7e40cb
Faisal Zaid <zaidalideveloper@gmail.com>
2026-03-05 09:48:35 +0300

    added bathrobes

---
+++ routes/bathrobes.py
@@ -0,0 +1,66 @@
+from flask import Blueprint, request, jsonify
+from models import db, Bathrobes
+
+bathrobes_bp = Blueprint("bathrobes_bp", __name__)
+
+# GET ALL
+@bathrobes_bp.route("/bathrobes", methods=["GET"])
+def get_bathrobes():
+    items = Bathrobes.query.all()
+    result = []
+
+    for item in items:
+        result.append({
+            "id": item.id,
+            "name": item.name,
+            "price": item.price,
+            "description": item.description,
+            "image": item.image
+        })
+
+    return jsonify(result), 200
+
+
+# POST
+@bathrobes_bp.route("/bathrobes", methods=["POST"])
+def add_bathrobes():
+    data = request.get_json()
+
+    new_item = Bathrobes(
+        name=data["name"],
+        price=data["price"],
+        description=data["description"],
+        image=data["image"]
+    )
+
+    db.session.add(new_item)
+    db.session.commit()
+
+    return jsonify({"message": "Bathrobe added"}), 201
+
+
+# PATCH
+@bathrobes_bp.route("/bathrobes/<int:id>", methods=["PATCH"])
+def update_bathrobes(id):
+    item = Bathrobes.query.get_or_404(id)
+    data = request.get_json()
+
+    item.name = data.get("name", item.name)
+    item.price = data.get("price", item.price)
+    item.description = data.get("description", item.description)
+    item.image = data.get("image", item.image)
+
+    db.session.commit()
+
+    return jsonify({"message": "Bathrobe updated"}), 200
+
+
+# DELETE
+@bathrobes_bp.route("/bathrobes/<int:id>", methods=["DELETE"])
+def delete_bathrobes(id):
+    item = Bathrobes.query.get_or_404(id)
+
+    db.session.delete(item)
+    db.session.commit()
+
+    return jsonify({"message": "Bathrobe deleted"}), 200
