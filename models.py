0ac452155ced62cb0ad784f7ff102f9402e561e7
Faisal Zaid <zaidalideveloper@gmail.com>
2026-03-12 11:41:03 +0300

    added admin verification

--- models.py
+++ models.py
@@ -5,20 +5,24 @@ db=SQLAlchemy()
 
 #ADMIN
 class Admin(db.Model):
-    __tablename__="admins"
+    __tablename__ = "admins"
 
-    id=db.Column(db.Integer, primary_key=True)
-    userName=db.Column(db.Text, unique=True, nullable =False)
-    password=db.Column(db.Text, unique=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
 
-      # Set password
+    first_name = db.Column(db.String(100), nullable=False)
+    last_name = db.Column(db.String(100), nullable=False)
+
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    password_hash = db.Column(db.String(200), nullable=False)
+
+    is_approved = db.Column(db.Boolean, default=False)
+
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
-    # Check password
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
-
 #products
 class Pajamas(db.Model):
     __tablename__="pajamas"   
