b2f62475c7cd364d2ffd22f6a19bc27a9a8f3f86
faisal ali zaid <zaidalideveloper@gmail.com>
2026-06-22 15:17:25 +0300

    added email confirmation message

---
+++ services/email_service.py
@@ -0,0 +1,30 @@
+from flask_mail import Message
+from app import mail
+
+
+def send_purchase_email(email, product):
+
+    msg = Message(
+        "Purchase Confirmation",
+        sender="myshop@gmail.com",
+        recipients=[email]
+    )
+
+    msg.body = f"""
+
+Hello,
+
+Thank you for purchasing:
+
+{product.name}
+
+Amount:
+Ksh {product.price}
+
+Your order has been received successfully.
+
+Thank you for shopping with TruHome.
+
+"""
+
+    mail.send(msg)
